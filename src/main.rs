ad061b448b67f154b57cca330979a5391168753a
AceDroidX <15519693+AceDroidX@users.noreply.github.com>
2026-05-24 05:54:19 +0000

    fix: 更新日志过滤器以提高hyper_util客户端的日志级别

--- src/main.rs
+++ src/main.rs
@@ -749,7 +749,7 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| EnvFilter::new("debug")),
+                .unwrap_or_else(|_| EnvFilter::new("debug,hyper_util::client::legacy::pool=info")),
         )
         .init();
 
